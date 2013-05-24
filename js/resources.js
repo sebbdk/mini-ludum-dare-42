b1cb9f3f17c4d49bc4d842a50b0a674f5e63ef60
kasper <barathrumm@gmail.com>
2013-05-25 00:20:47 +0200

    initial commit

---
+++ js/resources.js
@@ -0,0 +1,41 @@
+game.resources = [
+
+	/**
+	 * Graphics
+	 */
+	{name:'area01_level_tiles', type:'image', src:'data/img/maps/area01_level_tiles.png'},
+	// the main player spritesheet
+	{name: "gripe_run_right", type:"image", src: "data/img/sprites/gripe_run_right.png"},
+
+
+	/* 
+     * Maps. 
+     */
+	{name: "area00", type: "tmx", src: "data/map/area00.tmx"}
+
+	/* Graphics. 
+	 * @example
+	 * {name: "example", type:"image", src: "data/img/example.png"},
+	 */
+	
+	/* Atlases 
+	 * @example
+	 * {name: "example_tps", type: "tps", src: "data/img/example_tps.json"},
+	 */
+		
+	/* Maps. 
+	 * @example
+	 * {name: "example01", type: "tmx", src: "data/map/example01.tmx"},
+	 * {name: "example01", type: "tmx", src: "data/map/example01.json"},
+ 	 */
+
+	/* Background music. 
+	 * @example
+	 * {name: "example_bgm", type: "audio", src: "data/bgm/", channel : 1},
+	 */	
+	
+	/* Sound effects. 
+	 * @example
+	 * {name: "example_sfx", type: "audio", src: "data/sfx/", channel : 2}
+	 */
+];
